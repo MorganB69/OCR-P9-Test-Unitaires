70846a78415f02b29ea07ee725197a7f5d0ec000
morgan <brighi.morgan@wanadoo.fr>
2018-10-01 15:04:11 +0200

    Init du context spring pour test de db

--- myerp-consumer/src/test-consumer/java/com/dummy/myerp/testconsumer/consumer/TestITComptabiliteDaoImpl.java
+++ myerp-consumer/src/test-consumer/java/com/dummy/myerp/testconsumer/consumer/TestITComptabiliteDaoImpl.java
@@ -3,19 +3,34 @@ package com.dummy.myerp.testconsumer.consumer;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
+
 
 import com.dummy.myerp.consumer.dao.impl.db.dao.ComptabiliteDaoImpl;
 import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
 
+
 public class TestITComptabiliteDaoImpl {
 	
 	//CLASSE A TESTER
+	
 	ComptabiliteDaoImpl dao = ComptabiliteDaoImpl.getInstance();
+	
+	
+	@Before
+	public void setUp() {
+	SpringRegistry.init();
+		 
+
+	        
+	}
+
 	
 	@Test
 	public void testDaoNull() {
